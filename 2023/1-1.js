fa05029b2af1ea6a5e07d419b29d8ac3c36fbd14
Rachadjamal <sacha.mallet1@gmail.com>
2024-02-23 14:55:08 +0100

    Modify 2023 da

--- 2023/1-1.js
+++ 2023/1-1.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
-fs.readFile('./example', 'utf8', (err, input) => {
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err 
 
     const regex = /\D+/g;
     let data = input.split('\n').map(str => str.replace(regex, ''));
+    //console.log(data)
     let res = data.map(str => parseInt(str[0]+str[str.length-1]))
-    //console.log(res.reduce((a, b) => a + b))
+    console.log(res.reduce((a, b) => a + b))
 })
